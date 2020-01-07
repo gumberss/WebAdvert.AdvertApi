0e6233e0d0deeab07bcf2a3f3e4bd6efa4cd0612
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-07 15:04:17 -0300

    feat: criation of AdvertApi models

---
+++ AdvertApi.Models/AdvertModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AdvertApi.Models
+{
+    public class AdvertModel
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public double Price { get; set; }
+    }
+}
