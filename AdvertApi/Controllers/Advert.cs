18a71e3f84de249007736eedb9e41b58b90853cb
Gumbers <gustavomarinsuppi@gmail.com>
2020-01-07 15:44:10 -0300

    feat: creation of the controller class, and set the version in the route. In this case is 'api/v1/adverts'

---
+++ AdvertApi/Controllers/Advert.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/adverts")]
+    public class Advert : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
